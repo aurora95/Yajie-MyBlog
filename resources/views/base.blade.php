68f8f7bf134dcf88c83d564ef78c1c30dce7cdd4
aurora95 <yajie_xing@foxmail.com>
2015-12-04 15:35:35 +0800

    deleted readme

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -15,7 +15,6 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <!-- Scripts -->
     <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
     <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
